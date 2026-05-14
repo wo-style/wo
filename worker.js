50287623c152259335e313f03ce83de63ef25c8d
wo-style <email@wo.style>
2026-05-14 16:16:07 +0900

    update db url

--- worker.js
+++ worker.js
@@ -117,7 +117,7 @@ const start = async (sqlite3) => {
             await root.removeEntry(filename).catch(() => null);
             console.log("OPFSにデータベースは存在しませんでした");
             console.log("データベースをダウンロードします...");
-            const DB_URL = "https://pub-d666494efb334b1cab0884f65861efc4.r2.dev/wo.db";
+            const DB_URL = "https://db.wo.style/wo.db";
             const response = await fetch(DB_URL, { cache: "no-store" });
             if (!response.ok) {
                 throw new Error(`データベースのダウンロードに失敗しました： ${response.status}`);
